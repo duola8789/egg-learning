974649548c43b90620c188a05ad4ddd005228796
周昊 <zhouhoa8@xiaomi.com>
2019-05-05 18:43:40 +0800

    feat: 添加app.js

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,6 +4,9 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    // 通过ctx.helper获取helper中定义的方法
+    ctx.helper.format();
+    this.app.logger.info('debuger info1123');
     await ctx.render('index.tpl');
   }
 
---
+++ app/extend/helper.js
@@ -0,0 +1,9 @@
+'use strict';
+/**
+ * Created By zh on 2019-05-05
+ */
+module.exports = {
+  format() {
+    console.log('This is a helper method');
+  },
+};
